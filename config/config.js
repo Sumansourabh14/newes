3eb039de242930f70c562ecfd4e3d13b986d0f97
Suman Sourabh <suman.sourabh@ansrsource.com>
2024-05-04 00:30:32 +0530

    first commit

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const API_KEY = `19ac28c2bf434ae88bebd8c9813f4e40`;
+export const API_URL = `https://newsapi.org/v2`;
+export const TOP_HEADLINES_URL = `${API_URL}/top-headlines?country=in&apiKey=${API_KEY}`;
